eacb55c795d6638365da758d528dadd577761828
David Lapadula <lapadula.david@gmail.com>
2018-11-17 21:49:26 -0500

    implemented queue in javascript

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -13,19 +13,21 @@ function memoize(fn){
     const cache = {}; 
 
     return function(...args) {
+       
         if (cache[args]) {
             return cache[args]; 
         }
 
         const result = fn.apply(this, args); 
 
-        cache[args] = result; 
+        cache[args] = result;
 
         return result; 
     }
 }
 
 function fib(n) {
+
     if (n < 2) {
         return n; 
     }
@@ -34,6 +36,9 @@ function fib(n) {
 }
 
 fib = memoize(fib); 
+
+fib(4); 
+
 
 module.exports = fib;
 
