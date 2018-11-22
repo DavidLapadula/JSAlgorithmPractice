ddde5c90d41e5a18640bacd2b10f827ba85e4ed3
David Lapadula <lapadula.david@gmail.com>
2018-11-22 11:17:42 -0500

    using some linked list methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -4,18 +4,62 @@
 
 class Node {
     constructor(data, next = null) {
-        this.data = data; 
-        this.next = next; 
+        this.data = data;
+        this.next = next;
     }
 }
 
 class LinkedList {
     constructor() {
-        this.head = null; 
+        this.head = null;
     }
 
     insertFirst(data) {
-        this.head = new Node(data, this.head); 
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+
+            node = node.next;
+        }
+    }
+
+    clear() {
+        this.head = null; 
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        this.head = this.head.next; 
     }
 }
 
