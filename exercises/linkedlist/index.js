6f896a793b8059f7b6161a4c94b5de20598e281e
David Lapadula <lapadula.david@gmail.com>
2018-11-25 22:18:23 -0500

    implemented getAt and removeAt in linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -95,6 +95,40 @@ class LinkedList {
         }
     }
 
+    getAt(index) {
+
+        let counter = 0; 
+        let node = this.head; 
+
+        while(node) {
+            if (counter === index) {
+                return node; 
+            }
+
+            counter++; 
+            node = node.next; 
+        }
+
+        return null; 
+    }
+
+    removeAt(index) {
+        if (!this.head) {
+            return; 
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            return; 
+        }
+
+        const previous = this.getAt(index - 1); 
+        if (!previous || !previous.next) {
+            return; 
+        }
+        previous.next = previous.next.next; 
+    }
+
 }
 
 module.exports = { Node, LinkedList };
