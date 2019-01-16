04e1b289f740236dfe936a5c7473e962e030cd0f
David Work <lapadula.david@gmail.com>
2019-01-15 23:04:52 -0500

    implemented bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,21 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    // start implementation of bubble sort
+    for (let i = 0; i < arr.length; i++) {
+        // -1 added to ensure that it does not iterate out of bounds
+        // array 0 indexed so if minus by nothing, last element will be out of bounds
+        for (let j = 0; j < (arr.length - i - 1);  j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap the two values using a temp value
+                const lesser  = arr[j + 1] ;
+                arr[j + 1] = arr[j];
+                arr[j] = lesser; 
+            }
+        }
+    }
+    // return the sorted array
+    return arr;
 }
 
 function selectionSort(arr) {
